302da725f2ca0639d8c56046931033f3c00af23f
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2023-12-15 20:36:33 +0100

    feat: add scroll animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,22 @@ button.addEventListener('click', function() {
     button.classList.toggle('open')
     nav.classList.toggle('opened')
 })
+
+
+//intersection observer
+const hiddenElements = document.querySelectorAll('.hidden')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+        
+    })
+})
+
+hiddenElements.forEach((element) => {
+    observer.observe(element)
+})
